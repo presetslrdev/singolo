4998f00e14a5cc94c232f2477a311dabe94afc8e
presetslrdev <presetslr@gmail.com>
2020-03-21 20:50:01 +0300

    feat: add script
    add navigation link toggle active class

---
+++ script.js
@@ -0,0 +1,51 @@
+// navigation toggle
+const NAVIGATION = document.getElementById('navigation');
+NAVIGATION.addEventListener('click', navigationToggle);
+
+const HEADER = document.getElementById('header');
+let headerHeight = document.getElementById("header").clientHeight;
+const HEADER_HEIGHT_OFFSET = headerHeight;
+
+const removeActiveClass = () => {
+  NAVIGATION.querySelectorAll('.nav__link').forEach(item => {
+    item.classList.remove('active');
+  });
+};
+
+function navigationToggle(event) {
+  const navigationItem = event.target;
+  if (navigationItem.classList.contains('nav__link')) {
+    removeActiveClass();
+    navigationItem.classList.add('active');
+  }
+}
+
+function ChangeHeader() {
+  window.pageYOffset >= HEADER_HEIGHT_OFFSET
+    ? HEADER.classList.add("header__small")
+    : HEADER.classList.remove("header__small")
+}
+
+window.addEventListener("scroll", onChangeScroll);
+
+function onChangeScroll() {
+  ChangeHeader();
+
+  const servicesPosition = document.getElementById("services").offsetTop - headerHeight;
+  const portfolioPosition = document.getElementById("portfolio").offsetTop - headerHeight;
+  const aboutPosition = document.getElementById("about").offsetTop - headerHeight;
+  const contactPosition = document.getElementById("contact").offsetTop - headerHeight;
+
+  const currentPosition = window.pageYOffset;
+  if (currentPosition < servicesPosition) changeActiveClass(0);
+  else if (currentPosition >= servicesPosition && currentPosition < portfolioPosition) changeActiveClass(1);
+  else if (currentPosition >= portfolioPosition && currentPosition < aboutPosition) changeActiveClass(2);
+  else if (currentPosition >= aboutPosition && currentPosition < contactPosition && !(window.pageYOffset >= document.documentElement.offsetHeight - window.innerHeight)) changeActiveClass(3);
+  if (window.pageYOffset >= document.documentElement.offsetHeight - window.innerHeight || currentPosition >= contactPosition) changeActiveClass(4);
+}
+
+function changeActiveClass(item) {
+  removeActiveClass();
+  const navigationItems = NAVIGATION.querySelectorAll(".nav__link");
+  navigationItems[item].classList.add("active");
+}
